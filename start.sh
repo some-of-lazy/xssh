84e02cc5c26bff80f9a7c27f0a22b9c1417964ef
some-of-lazy <wuqi15054759669@outlook.com>
2023-09-28 20:48:42 +0800

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,10 +1,12 @@
-#!/bin/bash
-source /app/config.sh
+# #!/bin/bash
+# source /app/config.sh
 
-service ssh start
-service nginx start
+# service ssh start
+# service nginx start
 
-echo "set ngrok token: $NGROK_TOKEN"
-ngrok authtoken $NGROK_TOKEN
-echo "start ngrok service"
-ngrok tcp 22 --log=stdout > ngrok.log
+# echo "set ngrok token: $NGROK_TOKEN"
+# ngrok authtoken $NGROK_TOKEN
+# echo "start ngrok service"
+# ngrok tcp 22 --log=stdout > ngrok.log
+
+sudo docker run -i -t -d -p 80:80 --restart=always onlyoffice/documentserver
